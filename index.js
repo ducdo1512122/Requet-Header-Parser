47e0ebd2e0657bfe33754d1a73b294e1440a11c6
Duc Do <dotrananhduc@gmail.com>
2017-04-02 02:37:29 +0000

    finish get header parser

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/whoami', function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+  var info = {
+      'ipaddress': ip,
+      'language': req.headers["accept-language"].split(',')[0],
+      'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
+  };
+  res.send(info);
 })
 
 app.listen(8080, function () {
